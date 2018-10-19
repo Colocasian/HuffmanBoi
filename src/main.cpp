a8859ac12c51675a656187b8005f4ed7f199a0a6
Rishvic Pushpakaran <rishvic@localhost.localdomain>
2018-10-19 15:12:22 +0530

    removed broken implementation of huffman encoding (will do later)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include "../include/Huffman.hxx"
 
 int main()
 {
-    std::string yay("Hello Sir, my name is Chinappa");
-    Huffman encoder;
-    encoder.huffEncode(yay);
-    std::cout << encoder.getEncoded() << std::endl;
+    std::cout << "Not working for now" << std::endl;
+    return -1;
 }
