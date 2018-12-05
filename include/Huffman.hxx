e1545b8bb89fd5511e75740583c766effffa7ded
Colocasian <40887886+Colocasian@users.noreply.github.com>
2018-12-05 23:25:12 +0530

    cleaned up huffman.hxx

--- include/Huffman.hxx
+++ include/Huffman.hxx
@@ -4,25 +4,21 @@
 #include <string>
 #include <vector>
 
-class Huffman
-{
+class Huffman {
 private:
-    typedef struct HuffNode
-    {
+    typedef struct HuffNode {
         char c;
         struct HuffNode *right, *left;
     } HuffNode;
 
-    HuffNode *key;
+    HuffNode* key;
     std::string encoded;
     char rem;
 
-    class HuffQueue
-    {
+    class HuffQueue {
     private:
-        typedef struct QNode
-        {
-            HuffNode *_node;
+        typedef struct QNode {
+            HuffNode* _node;
             int _freq;
         } QNode;
         QNode arr[129];
@@ -33,7 +29,7 @@ private:
             int tint = arr[p1]._freq;
             arr[p1]._freq = arr[p2]._freq;
             arr[p2]._freq = tint;
-            HuffNode *tptr = arr[p1]._node;
+            HuffNode* tptr = arr[p1]._node;
             arr[p1]._node = arr[p2]._node;
             arr[p2]._node = tptr;
         }
@@ -41,8 +37,7 @@ private:
         void upMove(int at)
         {
             int p = at >> 1;
-            while (p)
-            {
+            while (p) {
                 if (arr[at]._freq < arr[p]._freq)
                     this->swap(at, p);
                 else
@@ -56,19 +51,15 @@ private:
         {
             int left = at << 1;
             int right = left + 1;
-            while (left <= this->top)
-            {
-                if (right == this->top + 1 ||
-                        arr[left]._freq <= arr[right]._freq)
-                {
+            while (left <= this->top) {
+                if (right == this->top + 1
+                    || arr[left]._freq <= arr[right]._freq) {
                     if (arr[at]._freq > arr[left]._freq)
                         this->swap(at, left);
                     else
                         break;
                     at = left;
-                }
-                else
-                {
+                } else {
                     if (arr[at]._freq > arr[right]._freq)
                         this->swap(at, right);
                     else
@@ -80,7 +71,7 @@ private:
             }
         }
 
-        void push(HuffNode *node, int freq)
+        void push(HuffNode* node, int freq)
         {
             top++;
             arr[top]._node = node;
@@ -101,13 +92,11 @@ private:
     public:
         HuffQueue() { top = 0; }
 
-        HuffNode *process(int freqArr[])
+        HuffNode* process(int freqArr[])
         {
-            for (int i = 0; i < 128; i++)
-            {
-                if (freqArr[i])
-                {
-                    HuffNode *ptr = new HuffNode;
+            for (int i = 0; i < 128; i++) {
+                if (freqArr[i]) {
+                    HuffNode* ptr = new HuffNode;
                     ptr->c = (char)i;
                     ptr->left = nullptr;
                     ptr->right = nullptr;
@@ -115,22 +104,18 @@ private:
                 }
             }
 
-            if (top == 1)
-            {
+            if (top == 1) {
                 QNode nod = this->pop();
-                HuffNode *ptr = new HuffNode;
+                HuffNode* ptr = new HuffNode;
                 ptr->c = '\0';
                 ptr->right = nullptr;
                 ptr->left = nod._node;
                 this->push(ptr, 0);
-            }
-            else
-            {
-                while (this->top != 1)
-                {
+            } else {
+                while (this->top != 1) {
                     QNode a = this->pop();
                     QNode b = this->pop();
-                    HuffNode *n = new HuffNode;
+                    HuffNode* n = new HuffNode;
                     n->c = '\0';
                     n->left = a._node;
                     n->right = b._node;
@@ -140,20 +125,19 @@ private:
             return this->pop()._node;
         }
     };
-    
-    void createBook(HuffNode *ptr, std::vector<std::string> &book, const std::string& res)
+
+    void createBook(
+        HuffNode* ptr, std::vector<std::string>& book, const std::string& res)
     {
         if (ptr == nullptr)
             return;
         if (ptr->c)
             book[(int)ptr->c] = res;
-        else
-        {
+        else {
             createBook(ptr->left, book, res + '0');
             createBook(ptr->right, book, res + '1');
         }
     }
-
 
 public:
     Huffman()
@@ -170,25 +154,23 @@ public:
             return;
 
         int arr[128];
-        for (char x: text)
+        for (char x : text)
             arr[(int)x]++;
-        
+
         HuffQueue grinder;
         this->key = grinder.process(arr);
         std::vector<std::string> book(128);
         this->createBook(this->key, book, "");
 
         int sz = 0;
-        for (char x: text)
+        for (char x : text)
             sz += book[(int)x].length();
         this->encoded.resize((sz + 7) / 8);
         this->rem = sz % 8;
 
         int at = 0;
-        for (char x: text)
-        {
-            for (char y: book[(int)x])
-            {
+        for (char x : text) {
+            for (char y : book[(int)x]) {
                 if (y == '1')
                     this->encoded[at / 8] |= (1 << (at % 8));
                 at++;
@@ -204,39 +186,32 @@ public:
         std::string ans("");
 
         char last;
-        if (this->rem)
-        {
+        if (this->rem) {
             last = this->encoded.back();
             this->encoded.pop_back();
         }
 
-        HuffNode *at = this->key;
-        for (char x: encoded)
-        {
-            for (int i = 0; i < 8; i++)
-            {
+        HuffNode* at = this->key;
+        for (char x : encoded) {
+            for (int i = 0; i < 8; i++) {
                 if (x & (1 << i))
                     at = at->right;
                 else
                     at = at->left;
-                if (at->c != '\0')
-                {
+                if (at->c != '\0') {
                     ans += at->c;
                     at = this->key;
                 }
             }
         }
 
-        if (this->rem)
-        {
-            for (int i = 0; i < this->rem; i++)
-            {
+        if (this->rem) {
+            for (int i = 0; i < this->rem; i++) {
                 if (last & (1 << i))
                     at = at->right;
                 else
                     at = at->left;
-                if (at->c != '\0')
-                {
+                if (at->c != '\0') {
                     ans += at->c;
                     at = this->key;
                 }
